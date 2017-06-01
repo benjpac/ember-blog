e258f548138ae87fe1353b8931aabcbc6eff887d
Ben Metzger and Chitroopa Manikkavasagam <ben+chitroopa@>
2017-06-01 09:38:09 -0700

    add comment model

---
+++ app/models/comment.js
@@ -0,0 +1,11 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+    author: DS.attr(),
+    content: DS.attr(),
+    created: DS.attr({
+        defaultValue(){
+        return new Date();
+        }
+    })    
+});
--- app/models/post.js
+++ app/models/post.js
@@ -8,5 +8,6 @@ export default DS.Model.extend({
     defaultValue(){
       return new Date();
     }
-  })
+  }),
+  comments: DS.hasMany('comment', { async: true })
 });
